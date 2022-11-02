172efe6463aff54a7e63e8b8424a7c09f047d085
KyleKinsella <C00273146@itcarlow.ie>
2022-11-02 17:23:32 +0000

    updated file

--- Lab5/Circle.java
+++ Lab5/Circle.java
@@ -1,8 +1,5 @@
 public class Circle extends Point{
     private int radius;
-    //private int c;
-    //private double half;
-    private int Z;
 
     public Circle() {
        this(0, 0, 0);
@@ -18,25 +15,7 @@ public class Circle extends Point{
     }
 
     public int getRadius() {
-        // formula = r = c/2 pie(3.14)
-        
-        /*c = 30;
-
-        radius = c /2;
-        half = radius * 3.14;
-        
-        */
-        
         return radius;
-    }
-
-
-    public void setZ(int Z) {
-        this.Z = Z;
-    }
-
-    public int getZ() {
-        return Z;
     }
 
     public String toString() {
